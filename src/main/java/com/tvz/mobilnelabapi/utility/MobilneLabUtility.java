95019efc54369a0b64464bb62a67eb76e34a9325
mkern <mkern@hbor.hr>
2018-07-23 13:42:31 +0200

    Dodan CSV reader.

--- src/main/java/com/tvz/mobilnelabapi/utility/MobilneLabUtility.java
+++ src/main/java/com/tvz/mobilnelabapi/utility/MobilneLabUtility.java
@@ -1,13 +1,37 @@
 package com.tvz.mobilnelabapi.utility;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
 
 public class MobilneLabUtility {
+	
+	 public static void convertFile() throws Exception {
+	        File input = new File("/x/data.csv");
+	        File output = new File("/x/data.json");
+
+	        List<Map<?, ?>> data = readObjectsFromCsv(input);
+	        writeAsJson(data, output);
+	    }
+		
+	 public static List<Map<?, ?>> readObjectsFromCsv(File file) throws IOException {
+		 CsvMapper mapper = new CsvMapper();
+		 CsvSchema schema = CsvSchema.emptySchema().withHeader();
+	        MappingIterator<Map<?, ?>> mappingIterator = mapper.readerFor(Map.class).with(schema).readValues(file);
+
+	        return mappingIterator.readAll();
+	    }
+
+	    public static void writeAsJson(List<Map<?, ?>> data, File file) throws IOException {
+	        ObjectMapper mapper = new ObjectMapper();
+	        mapper.writeValue(file, data);
+	    }
 	
 }
